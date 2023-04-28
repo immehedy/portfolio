f5862a9e5170876095a18dabfa58063de60f755c
mehedy hassan <dev.mehedy.hassan@gmail.com>
2023-04-29 01:27:36 +0600

    avatar image desing added

--- src/components/layout/Header/index.tsx
+++ src/components/layout/Header/index.tsx
@@ -4,7 +4,7 @@ import React from "react";
 const Header = () => {
   return (
     <div className="z-50 right-0 top-0 py-4 px-8">
-      <ul className="text-gray-300 font-semibold flex justify-end gap-2">
+      <ul className="text-gray-300 font-semibold flex justify-end gap-8">
         <li>
           {" "}
           <Link href="/about">About</Link>
